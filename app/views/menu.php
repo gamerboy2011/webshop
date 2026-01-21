f9471c30421c71961f290f848a717e19c46667ba
gamerboy2011 <faludi.balint08@gmail.com>
2026-01-21 12:18:49 +0100

    Add main layout structure and include necessary scripts and styles

--- app/views/menu.php
+++ app/views/menu.php
@@ -1,5 +1,33 @@
-<menu class="flex w-full ">
-    <div class="logo flex justify-start"></div>
+<menu class="flex w-full justify-between ">
+    <div class="p-2 main-menu w-full flex justify-strech">
+    <div class="categories-menu  flex items-center items-center">
+        <div class="py-3  px-1 cursor-pointer hover:border-b-2 border-solid">Férfi</div>
+        <div class="py-3 px-1 cursor-pointer hover:border-b-2 border-solid">Női</div>
+        <div class="py-3 px-1 cursor-pointer hover:border-b-2 border-solid">Gyerek</div>
+    </div>
+    <!--
+    m: top ,right, bottom
+    mx: left,right
+    my: top, bottom
+    ml:left
+    mr:right
+    mt:top
+    mb:bottom
+    Ugyan ez a padding csak p-vel
+     -->
+    <div class="logo mx-2 flex items-center">
+        <span class="bg-black p-1 text-white mr-1">Fantasy</span>
+        <span class="bg-black p-1 text-white"> Shop</span>
+    </div>
+    <div class="icons flex items-center">
+        <i class="las la-bell"></i>
+        <i class="las la-percent"></i>
+        <i class="lar la-heart"></i>
+        <i class="las la-shopping-bag"></i>
+        <i class="las la-user"></i>
+    </div>
+    </div>
+
     <div class="menu-points flex justify-end"></div>
     <div>Főoldal</div>
     <div>Termékek</div>
@@ -7,3 +35,9 @@
     <div>Regisztrálok</div>  
     
 </menu>
+<div class="flex w-full justify-between">
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 include("./app/views/head.php");
+include("./app/views/footer.php");
+include("./app/views/main/main.php");
 include("./app/views/menu.php");
 include("./app/views/footer.php");
 ?>
